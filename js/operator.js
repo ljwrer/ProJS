0ee61e45359a1a31aea3dba8312f6b0f93a055a7
ljwrer <ljwrer@sina.com>
2015-06-03 00:08:06 +0800

    Boolean Object Number String

---
+++ js/operator.js
@@ -0,0 +1,73 @@
+//var a="hi"||"hello";
+//alert(a);
+//var b=Infinity*0;
+//alert(b);
+//var c=10/3;
+//alert(c);
+//alert(Infinity/0);
+//alert(5.2%2.5);
+//alert(Infinity%5);
+//alert(Number("a5"));
+//var i=0,j=0;
+//for(var name in window)
+//{
+//	document.write(name+"<br>");
+//	i++;
+//}
+//document.write("---------------------------------------------"+"<br>");
+//for(var name in document)
+//{
+//	document.write(name+"<br>");
+//	j++;
+//}
+//document.write("i="+i+",j="+j);
+//var num=0;
+//outermost:
+//for(var i=0;i<10;i++)
+//{
+//	for(var j=0;j<10;j++)
+//	{
+//		if(i==5&&j==5)
+//		{
+//			continue outermost;
+//		}
+//		num++;
+//	}
+//}
+//alert(num);
+//function sum(a,b){
+//	return a+b;
+//}
+//alert(sum(1,2));
+//alert(sum("1","2"));
+//function add(num1,num2){
+//	"use strict";
+//	arguments[1]=10;
+//	arguments[2]=10;
+//	alert(arguments[0]+num2);
+//}
+//add(1);
+//var a=1;
+//function f(){
+//	"use strict";
+//}
+//var a;
+//var b=undefined;
+//alert(typeof a);//undefined
+//alert(c) //undefined
+//var a=null;
+//alert(typeof a);
+//var a="ok"
+//if(a)
+//{
+//	alert(a);
+//}
+//var a =0.1,b=0.2;
+//alert(a+b);
+//var a=3.14e5;
+//alert(a);
+//var a=3.14e-3;
+//alert(a);
+//alert(Number.MAX_VALUE);alert(Number.MIN_VALUE);isFinite();Number.POSITIVE_INFINITY,Number.NEGATIVE_INFINITY
+alert(NaN!=NaN);isNaN()
+
