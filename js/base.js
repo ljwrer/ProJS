cf34dbdf90375cc2ed116de6c2a8a0f03a2cfaa8
ljwrer <ljwrer@sina.com>
2015-06-08 09:39:29 +0800

    ReferenceType

--- js/base.js
+++ js/base.js
@@ -1,3 +1,41 @@
 function write(value){
 	document.write(value+"<br>");
 }
+function type(value){
+	var result;
+	if(typeof value!=="object")
+	{
+		result=typeof value;
+	}
+	else if(value===null)
+	{
+		result="null";
+	}else 
+	{
+		switch(true)
+		{
+			case value instanceof Array:
+				result="Array";
+				break;
+			case value instanceof RegExp:
+				result="RegExp";
+				break;
+			case value instanceof Date :
+				result="Date";
+				break;
+			case value instanceof Math:
+				result="Math";
+				break;
+			case value instanceof Function:
+				result="Function";
+				break;
+			case value instanceof Event:
+				result="Event";
+				break;
+			default:
+				result="unKnow";
+				break;
+		}
+	}
+	return result;
+}
